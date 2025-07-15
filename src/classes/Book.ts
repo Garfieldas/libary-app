da871f3ad05adf8ffd03062dbb069d7d160eeca8
Garieldas <dainiusrainys99@gmail.com>
2025-07-15 23:10:06 +0300

    Removed interface. created book class

---
+++ src/classes/Book.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+export class Book  {
+    id: string;
+    title: string;
+    author: string;
+    pages: number;
+    status: boolean
+
+  constructor(id: string, title: string, author: string, pages: number, status: boolean) {
+    this.id = uuidv4();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+  }
+  
+}
