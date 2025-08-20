6be6807280ae44f23e7de68a0ac3f61886280f2b
Garieldas <dainiusrainys99@gmail.com>
2025-08-20 22:13:46 +0300

    Edited book class. Wrapped adding new card component inside try catch block

--- src/classes/Book.ts
+++ src/classes/Book.ts
@@ -1,12 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
 export class Book  {
-    id: string;
+    id?: string;
     title: string;
     author: string;
     pages: number;
     status: boolean
 
-  constructor(id: string, title: string, author: string, pages: number, status: boolean) {
+  constructor(title: string, author: string, pages: number, status: boolean) {
     this.id = uuidv4();
     this.title = title;
     this.author = author;
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,32 @@
+import { BookCard } from "./components/BookCard";
 import { useModal } from "./composable/modal";
 
-const { showModal, closeModal, addBtn, overlay, getFormValues, submitBtn  } = useModal();
+const { showModal, closeModal, addBtn, overlay, getFormValues, submitBtn } =
+  useModal();
 
-addBtn?.addEventListener('click', () => {
-    showModal();
+addBtn?.addEventListener("click", () => {
+  showModal();
 });
 
-overlay?.addEventListener('click', () => {
-    closeModal();
+overlay?.addEventListener("click", () => {
+  closeModal();
 });
 
-submitBtn?.addEventListener('click', (e) => {
-    e.preventDefault();
-    const { title, author, pages } = getFormValues();
-    if (!title || !author || !pages) {
-        alert('All fields are required');
+submitBtn?.addEventListener("click", (e) => {
+  e.preventDefault();
+  const { title, author, pages } = getFormValues();
+  if (!title || !author || !pages) {
+    alert("All fields are required");
+  }
+  try {
+    if (title && author && pages) {
+      const pageNumber = parseInt(pages);
+      const status = false;
+      BookCard({ title, author, pages: pageNumber, status });
+      closeModal();
     }
-})
+  } catch (error: any) {
+    alert('Something went wrong');
+    closeModal();
+  }
+});
