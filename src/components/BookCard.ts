1e62c29f40c06e2b9b1d3e08ee09ed8dd7e761f7
Garieldas <dainiusrainys99@gmail.com>
2025-08-20 22:25:35 +0300

    Refactored book card component

--- src/components/BookCard.ts
+++ src/components/BookCard.ts
@@ -9,21 +9,24 @@ export const BookCard = (book: Book) => {
     const i = document.createElement('i');
     i.className = 'fa-solid fa-bookmark';
     h3.appendChild(i);
-    h3.textContent = 'Book title';
+    const title = document.createTextNode(' Book title')
+    h3.appendChild(title);
     card.appendChild(h3);
 
     const p1 = document.createElement('p');
     const strong1 = document.createElement('strong');
-    strong1.textContent = 'Author';
-    p1.textContent = 'John Doe';
+    strong1.textContent = 'Author: ';
     p1.appendChild(strong1);
+    const author = document.createTextNode('John Doe')
+    p1.appendChild(author);
     card.appendChild(p1);
 
     const p2 = document.createElement('p');
     const strong2 = document.createElement('strong');
-    strong2.textContent = 'Pages';
-    p2.textContent = '100';
+    strong2.textContent = 'Pages: ';
+    const pages = document.createTextNode('100')
     p2.appendChild(strong2);
+    p2.appendChild(pages);
     card.appendChild(p2);
 
     const buttonsContainer = document.createElement('div');
